c3c21e45d11a8e2738b6784167414114afd293fe
Faris Mohammed <farismosman@gmail.com>
2016-12-06 13:49:17 +0200

    uses basic spark image to build predictionio image

--- urengine/train_and_deploy.sh
+++ urengine/train_and_deploy.sh
@@ -8,5 +8,5 @@ echo "${date} Training and deploying PredictionIO engine\\n"
 # Train and deploy
 
 pio build --verbose
-pio train -- --master spark://masterspark:7077 --executor-memory 4G --driver-memory 4G --conf spark.serializer=org.apache.spark.serializer.KryoSerializer
+pio train -- --master spark://piosparkmaster:7077 --executor-memory 4G --driver-memory 4G --conf spark.serializer=org.apache.spark.serializer.KryoSerializer
 pio deploy -- --executor-memory 4G --driver-memory 4G --total-executor-cores 2
